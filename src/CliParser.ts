df8fa5f566e282dcb37ebc12123fea7e0fc56a44
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2021-08-06 21:51:33 -0700

    Set up a test harness

--- src/CliParser.ts
+++ src/CliParser.ts
@@ -1,4 +1,13 @@
+import { ICliSpec } from './CliSpec';
+
+export interface ICliParserResult {}
 
 export class CliParser {
+  public static createFromSpec(cliSpec: ICliSpec): CliParser {
+    return new CliParser();
+  }
 
+  public parse(input: string): ICliParserResult {
+    return {};
+  }
 }
--- src/test/CliParser.test.ts
+++ src/test/CliParser.test.ts
@@ -1,12 +1,38 @@
 import * as path from 'path';
-import { JsonFile, JsonObject } from '@rushstack/node-core-library'
+import { JsonFile } from '@rushstack/node-core-library';
+import { ICliSpec } from '../CliSpec';
+import { CliParser } from '../CliParser';
 
-const cliSpecsPath: string = path.join(__dirname, 'cli-specs.json');
+export interface ITestProfile {
+  testName: string;
+  testInputs: string[];
+  spec: ICliSpec;
+}
 
-const cliSpecs: JsonObject = JsonFile.load(cliSpecsPath);
+export interface ITestProfileSet {
+  testProfiles: ITestProfile[];
+}
 
-describe('CliParser Test', () => {
-  it('Correctly tests stuff', () => {
-    expect(cliSpecs).toMatchSnapshot();
-  });
+describe('CliParser', () => {
+  const testDataPath: string = path.join(__dirname, 'test-profiles.json');
+  const testProfileSet: ITestProfileSet = JsonFile.load(testDataPath);
+
+  for (const testProfile of testProfileSet.testProfiles) {
+    let cliParser: CliParser | undefined = undefined;
+
+    test('parseSpec', () => {
+      cliParser = CliParser.createFromSpec(testProfile.spec);
+    });
+
+    test(testProfile.testName, () => {
+      if (cliParser) {
+        for (const testInput of testProfile.testInputs) {
+          expect({
+            _testInput: testInput,
+            result: cliParser.parse(testInput)
+          }).toMatchSnapshot();
+        }
+      }
+    });
+  }
 });
